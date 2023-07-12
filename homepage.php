8b495a675f4203acd4ef79a19d8044c47f748ae4
Muhammad Harits <100942834+mharits-s@users.noreply.github.com>
2023-07-12 20:50:40 +0700

    Add files via upload

---
+++ homepage.php
@@ -0,0 +1,320 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kasipaham | Bikin EZ aja!</title>
+    <link rel="icon" type="icon" href="./assets/Kasipaham ico.svg">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <script src="https://cdn.lordicon.com/bhenfmcm.js"></script>
+    <link rel="stylesheet" href="style.css">
+    <style>
+        body{
+            margin: 0;
+        }
+        .navbar{
+            display: flex;
+            padding: 19px 64px;
+            justify-content: center;
+            align-items: center;
+            position: fixed;
+            gap: 368px;
+            top: 0%;
+            left: 0;
+            right: 0;
+            background: #0096C7;
+            z-index: 100;
+            border-radius: 0px 0px 25px 25px;
+        }
+        .items{
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            gap: 32px;
+            color: #FAFAFA;
+        }
+        .items p{
+            margin-bottom: 0;
+        }
+        .items p:hover{
+            border-bottom: 2px solid #FFD500;
+        }
+        a{
+            display: flex;
+            padding: 10px 15px;
+            justify-content: center;
+            align-items: center;
+            gap: 10px;
+            background-color: #0096C7;
+            color: #FAFAFA;
+            text-decoration: none;
+            border: 1px solid var(--neutral-100, #FAFAFA);
+        }
+        a:hover{
+            color: #0096C7;
+            background: #FAFAFA;
+            box-shadow: 0px 4px 10px 0px rgba(0, 0, 0, 0.20);
+        }
+        .image-container{
+            background: var(--grayish, #EDF2FB);
+            margin-top: 59px;
+        }
+        .image-container img{
+            display: block;
+            width: 100%;
+            justify-content: center;
+            align-items: center;
+            flex-shrink: 0;
+        }
+        .promo-container{
+            display: flex;
+            width: 100%;
+            height: 500px;
+            justify-content: center;
+            align-items: center;
+            flex-shrink: 0;
+            background: var(--grayish, #EDF2FB);
+        }
+        .promo-container img{
+            height: 240px;
+            flex-shrink: 0;
+        }
+        .content-container{
+            display: flex;
+            width: 100%;
+            height: relative;
+            justify-content: center;
+            flex-direction: column;
+            align-items: center;
+            flex-shrink: 0;
+            padding: 64px 64px;
+            background: var(--light, #F8F7FF);
+        }
+        .content-container h1{
+            font-size: 32px;
+            color: #0096C7;
+        }
+        .fill-container {
+            display: flex;
+            margin-top: 46px;
+            flex-wrap: wrap;
+            justify-content: space-between;
+            align-items: top;
+        }
+        .content{
+            width: calc(33.33% - 56px);
+            margin-bottom: 20px;
+            display: flex;
+            flex-direction: column;
+            align-items: flex-start;
+        }
+        .title-content{
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+        .title-content lord-icon{
+            height: 72px;
+            width: 72px;
+            margin-right: 6px;
+        }
+        .title-content h2{
+            color: #023E8A;
+            margin-bottom: 0;
+        }
+        .content p{
+            margin-left: 76px;
+            margin-top: -16px;
+            color: #737373;
+        }
+        footer{
+            display: flex;
+            width: 100%;
+            padding: 96px 190px;
+            align-items: flex-start;
+            gap: 96px;
+            background: #0096C7;
+        }
+        .app{
+            display: flex;
+            flex-direction: column;
+            align-items: flex-start;
+            gap: 32px;
+        }
+        .store{
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        gap: 16px;
+        }
+        .icon-store{
+        display: flex;
+        align-items: flex-start;
+        gap: 21px;
+        }
+        .icon-store img{
+        display: flex;
+        align-items: flex-start;
+        height: 48px;
+        }
+        .information{
+            display: flex;
+            flex-direction: column;
+            align-items: flex-start;
+        }
+        .information h1 {
+        border-bottom: 3px solid #FFD500;
+        margin-bottom: 18px;
+        }
+        .info-content{
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            gap: 16px;
+            margin-bottom: 16px;
+        }
+        .info-content svg{
+            width: 28px;
+            height: 28px;
+        }
+        .info-content p{
+            margin-bottom: 0;
+            margin-left: 5px;
+        }
+        .copyrights{
+        display: flex;
+        height: 48px;
+        padding: 10px 0px;
+        justify-content: center;
+        align-items: center;
+        background: #023E8A;
+        }
+        .copyrights p{
+            margin-bottom: 0;
+        }
+    </style>
+</head>
+<body>
+    <div class="navbar">
+        <img src="./assets/kasipaham.svg" alt="kasipaham logo">
+        <div class="items">
+            <p>Produk</p>
+            <p>Paket Belajar</p>
+            <p>Tentang Kami</p>
+        </div>
+        <a type="submit" href="login.php">Masuk/Daftar</a>
+    </div>
+    <div class="image-container">
+        <img src="./assets/Title-Header.png" alt="Header">
+    </div>
+    <div class="promo-container">
+        <img src="./assets/card tryout.png" alt="Tryout">
+    </div>
+    <div class="content-container">
+        <h1>Kenapa harus #KasipahamAja?</h1>
+        <div class="fill-container">
+            <div class="content">
+                <div class="title-content">
+                    <lord-icon
+                    src="https://cdn.lordicon.com/dxoycpzg.json"
+                    trigger="loop"
+                    delay="2000"
+                    colors="primary:#f24c00,secondary:#646e78,tertiary:#4bb3fd,quaternary:#ebe6ef,quinary:#f9c9c0">
+                </lord-icon>
+                <h2>Materi Asique <br></h2>
+                </div>
+                <p>Ya dong, dengan Kasipaham, materi yang dikasih pasti selalu asqiue</p>
+            </div>
+            <div class="content">
+                <div class="title-content">
+                    <lord-icon
+                    src="https://cdn.lordicon.com/ajkxzzfb.json"
+                    trigger="loop"
+                    delay="2000"
+                    colors="primary:#ffc738,secondary:#4bb3fd"
+                    state="hover-looking-around">
+                </lord-icon>
+                <h2>Mentor Ketjeh <br></h2>
+                </div>
+                <p>Dengan mentor yang ketjeh badai, belajar kalian lebih menyenangkan
+                    dan seru. Kesusahan dengan materi? Mentor Kasipaham bakal kasih paham kalian
+                    dengan sepenuh hati dong.
+                </p>
+            </div>
+            <div class="content">
+                <div class="title-content">
+                    <lord-icon
+                    src="https://cdn.lordicon.com/oqhlhtfq.json"
+                    trigger="loop"
+                    delay="2000"
+                    colors="primary:#4bb3fd,secondary:#ebe6ef"
+                    state="hover-2">
+                </lord-icon>
+                <h2>Support 24/7 <br></h2>
+                </div>
+                <p>Dengan support 24/7, kalian bisa mengakses Kasipaham kapan aja dan dimana aja loh.
+                    So, jaman now belajar bisa lebih EZ PZ ya.
+                </p>
+            </div>
+        </div>
+    </div>
+    <footer>
+        <div class="app">
+            <img src="./assets/kasipaham.svg" alt="kasipaham logo">
+            <div class="store">
+                <h1>Mobile Version</h1>
+                <div class="icon-store">
+                <img src="./assets/playstore.png" alt="playstore">
+                <img src="./assets/app store.png" alt="app store">
+                </div>
+            </div>
+        </div>
+        <div class="information">
+            <h1>Perusahaan</h1>
+            <p>Tentang Kami</p>
+            <p>Karir</p>
+        </div>
+        <div class="information">
+            <h1>Ikuti Kami</h1>
+            <div class="info-content">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
+                    <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"/>
+                  </svg>
+                <p>Facebook</p>
+            </div>
+            <div class="info-content">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-instagram" viewBox="0 0 16 16">
+                    <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.927 3.927 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 0 0-.923-1.417A3.911 3.911 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0h.003zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599.28.28.453.546.598.92.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.47 2.47 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.478 2.478 0 0 1-.92-.598 2.48 2.48 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233 0-2.136.008-2.388.046-3.231.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92.28-.28.546-.453.92-.598.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045v.002zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92zm-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217zm0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334z"/>
+                  </svg>
+                <p>Instagram</p>
+            </div>
+            <div class="info-content">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-twitter" viewBox="0 0 16 16">
+                    <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z"/>
+                  </svg>
+                <p>twitter</p>
+            </div>
+        </div>
+        <div class="information">
+            <h1>Kontak Kami</h1>
+            <div class="info-content">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-whatsapp" viewBox="0 0 16 16">
+                    <path d="M13.601 2.326A7.854 7.854 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.933 7.933 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.898 7.898 0 0 0 13.6 2.326zM7.994 14.521a6.573 6.573 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.557 6.557 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592zm3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.729.729 0 0 0-.529.247c-.182.198-.691.677-.691 1.654 0 .977.71 1.916.81 2.049.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232z"/>
+                  </svg>
+                <p>Whatsapp</p>
+            </div>
+            <div class="info-content">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope-at" viewBox="0 0 16 16">
+                    <path d="M2 2a2 2 0 0 0-2 2v8.01A2 2 0 0 0 2 14h5.5a.5.5 0 0 0 0-1H2a1 1 0 0 1-.966-.741l5.64-3.471L8 9.583l7-4.2V8.5a.5.5 0 0 0 1 0V4a2 2 0 0 0-2-2H2Zm3.708 6.208L1 11.105V5.383l4.708 2.825ZM1 4.217V4a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v.217l-7 4.2-7-4.2Z"/>
+                    <path d="M14.247 14.269c1.01 0 1.587-.857 1.587-2.025v-.21C15.834 10.43 14.64 9 12.52 9h-.035C10.42 9 9 10.36 9 12.432v.214C9 14.82 10.438 16 12.358 16h.044c.594 0 1.018-.074 1.237-.175v-.73c-.245.11-.673.18-1.18.18h-.044c-1.334 0-2.571-.788-2.571-2.655v-.157c0-1.657 1.058-2.724 2.64-2.724h.04c1.535 0 2.484 1.05 2.484 2.326v.118c0 .975-.324 1.39-.639 1.39-.232 0-.41-.148-.41-.42v-2.19h-.906v.569h-.03c-.084-.298-.368-.63-.954-.63-.778 0-1.259.555-1.259 1.4v.528c0 .892.49 1.434 1.26 1.434.471 0 .896-.227 1.014-.643h.043c.118.42.617.648 1.12.648Zm-2.453-1.588v-.227c0-.546.227-.791.573-.791.297 0 .572.192.572.708v.367c0 .573-.253.744-.564.744-.354 0-.581-.215-.581-.8Z"/>
+                  </svg>
+                <p>cs@kasipaham.com</p>
+            </div>
+        </div>
+    </footer>
+    <div class="copyrights">
+        <p>©2023 - Kasipaham. All rights reserved.</p>
+    </div>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
