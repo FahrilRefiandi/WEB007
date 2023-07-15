f4f5a82c1d880a27a67950d9f00f0c5451bf1eea
mharits.s <mharits.beyourself@gmail.com>
2023-07-15 08:06:14 +0700

    modified:   homepage.php

--- homepage.php
+++ homepage.php
@@ -6,10 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Kasipaham | Bikin EZ aja!</title>
     <link rel="icon" type="icon" href="https://raw.githubusercontent.com/FahrilRefiandi/WEB007/main/assets/images/Kasipaham%20ico.svg?token=GHSAT0AAAAAACFACY64WPZQCX6XWAEREESAZFR6TCQ">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <script src="https://cdn.lordicon.com/bhenfmcm.js"></script>
-    <link rel="stylesheet" href="https://raw.githubusercontent.com/FahrilRefiandi/WEB007/main/assets/style.css?token=GHSAT0AAAAAACFACY65QRWMDMVTSWZR3AEUZFR6ZUA">
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Nunito">
+    <link rel="stylesheet" href="./assets/style.css">
     <style>
         body {
             margin: 0;
