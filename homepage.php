41721d9ac0cb59285d3fd8b81e21309fdac497bc
mharits.s <mharits.beyourself@gmail.com>
2023-07-15 07:19:09 +0700

    modified:   homepage.php

--- homepage.php
+++ homepage.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Kasipaham | Bikin EZ aja!</title>
-    <link rel="icon" type="icon" href="./assets/Kasipaham ico.svg">
+    <link rel="icon" type="icon" href="https://github.com/FahrilRefiandi/WEB007/blob/main/assets/images/Kasipaham%20ico.svg?raw=true">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <script src="https://cdn.lordicon.com/bhenfmcm.js"></script>
     <link rel="stylesheet" href="style.css">
