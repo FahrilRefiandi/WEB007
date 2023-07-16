427ec14e27d8aa966cb829f079eeda88fe65847d
mharits.s <mharits.beyourself@gmail.com>
2023-07-16 16:19:49 +0700

    modified:   assets/style.css 	new file:   page/student-mentor/dashboard.php 	new file:   page/student-mentor/layouts/footer.css 	new file:   page/student-mentor/layouts/footer.php 	new file:   page/student-mentor/layouts/header.css 	new file:   page/student-mentor/layouts/header.php 	deleted:    page/student/mentor/dashboard.php

---
+++ page/student-mentor/dashboard.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard Kasipaham</title>
+    <link rel="icon" type="icon" href="../../assets/images/Kasipaham ico.svg">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <script src="https://cdn.lordicon.com/bhenfmcm.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Nunito">
+    <link rel="stylesheet" href="../../assets/style.css">
+</head>
+<body>
+    <?php include 'layouts/header.php'; ?>
+    
+    <?php include 'layouts/footer.php'; ?>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>    
+</body>
+</html>
---
+++ page/student-mentor/layouts/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Header</title>
+    <link rel="stylesheet" type="text/css" href="layouts/header.css">
+</head>
+<body>  
+<div class="navbar-custom">
+        <img src="../../assets/images/kasipaham.svg" alt="kasipaham logo">
+        <p>Hi, User! [Isi nama user dari DB kasipahamv2, tabel users, kolom name]</p>
+    </div>
+</body>
+</html>
